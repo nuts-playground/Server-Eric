eb93dfb820237e8afc25d8df88c6d20a730aca3f
seokho <rkwhr3256@naver.com>
2023-12-11 01:09:41 +0900

    test: 환경 변수 로드 실패로 스크립트 수정

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -1,8 +1,21 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const envFilePath = path.join(__dirname, 'env', `.${process.env.NODE_ENV}.env`);
+const result = dotenv.config({ path: envFilePath });
+
+if (result.error) {
+  throw result.error;
+}
+
 module.exports = [
   {
     script: 'dist/main.js',
     name: 'server',
     exec_mode: 'cluster',
     instances: 2,
+    env: {
+      NODE_ENV: process.env.NODE_ENV || 'production', // 기본값 설정
+      ...result.parsed,
+    },
   },
 ];
