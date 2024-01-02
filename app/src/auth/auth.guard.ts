c9ba82cb51da93bb0410c8dcc5c4b2bbec3c5f8d
seokho7 <rkwhr3256@naver.com>
2024-01-02 22:25:03 +0900

    feat: 소셜 로그인 추가
    구글, 깃허브, 네이버, 카카오

---
+++ app/src/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class GoogleAuthGuard extends AuthGuard('google') {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request);
+        return result;
+    }
+}
+
+@Injectable()
+export class GithubAuthGuard extends AuthGuard('github') {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request);
+        return result;
+    }
+}
+
+@Injectable()
+export class NaverAuthGuard extends AuthGuard('naver') {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request);
+        return result;
+    }
+}
+
+@Injectable()
+export class KakaoAuthGuard extends AuthGuard('kakao') {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request);
+        return result;
+    }
+}
