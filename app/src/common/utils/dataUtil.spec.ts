b9f87d76c511b70e8e9d9360af7a79088c3c36ec
seokho7 <rkwhr3256@naver.com>
2024-01-06 00:24:16 +0900

    test: ㅜㅜ oauth 이상

--- app/src/common/utils/dataUtil.spec.ts
+++ app/src/common/utils/dataUtil.spec.ts
@@ -1,8 +1,7 @@
 import { convert, DateTimeFormatter, LocalDate, LocalDateTime, nativeJs } from '@js-joda/core';
 describe('공통 데이트 유틸 테스트', () => {
-    it('현재 시간 가져오기',()=>{
+    it('현재 시간 가져오기', () => {
         const dbDate = LocalDate;
         expect(dbDate).toEqual(LocalDate);
-
-    })
-})
+    });
+});
--- app/src/main/main.service.ts
+++ app/src/main/main.service.ts
@@ -4,9 +4,7 @@ import { UserService } from '../user/user.service';
 
 @Injectable()
 export class MainService {
-    constructor(
-        private readonly userService: UserService
-    ) {}
+    constructor(private readonly userService: UserService) {}
     async returnUser(userEmail: string): Promise<User | boolean> {
         try {
             const user = await this.userService.findByEmail(userEmail);
